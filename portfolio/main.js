2faf001c00cdf6a1a04fe1e9287648cd251c14c0
autonolan <teasdale07@gmail.com>
2023-11-09 22:30:18 -0800

    make project image display in absoulte position on screen on top of a semi transparent black background. Click image to hide.

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,11 +1,29 @@
 //let lhpItem = document.getElementById("lhp-item");
+console.log(document.getElementById("lhp-item"));
 let projectsList = document.getElementById("projects-list").children;
 console.log(projectsList);
 function showProject(event) {
     event.target.children[1].style.display = 'block';
+    event.target.children[1].style.position = 'absolute';
+    event.target.children[1].style.left = '300px';
+    event.target.children[1].style.top = '300px';
+    document.getElementById("main-content").style.backgroundColor = 'rgba(0,0,0,0.6)';
 };
+function hideProject() {
+    document.getElementById("lhp").style.display = 'none';
+    document.getElementById("lhp").style.position = '';
+    document.getElementById("tji").style.display = 'none';
+    document.getElementById("tji").style.position = '';
+    document.getElementById("pst").style.display = 'none';
+    document.getElementById("pst").style.position = '';
+    document.getElementById("main-content").style.backgroundColor = '';
+}
 Array.from(projectsList).forEach(project => {
     console.log(project);
     project.addEventListener('click', showProject);
-})
+});
+document.getElementById("lhp").addEventListener('click', hideProject);
+document.getElementById("tji").addEventListener('click', hideProject);
+document.getElementById("pst").addEventListener('click', hideProject);
 //lhpItem.addEventListener('click', showProject);
+console.log(document.getElementById("lhp-item"));
