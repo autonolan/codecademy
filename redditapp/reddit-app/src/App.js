fccfc0892137e6204c230bf0342a5a1085e83849
autonolan <144947920+autonolan@users.noreply.github.com>
2024-09-28 14:22:19 -0700

    Redditapp (#7)
    * react redux install
    
    * reddit-app app.js changes

---
+++ redditapp/reddit-app/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Best of Reddit App</h1>
+        <Counter />
+        <p>
+          uses Reddit JSON Api to only display the all time high from any subreddit and creates a feed of all time all time highs.
+          User can favorite these posts which will be displayed in their favorites section so they immediately see their favorite best of
+          the best. For each post, only the top comment will be displayed. The user will be able to show or hide that comment if they
+          agree or disagree with the comment.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
